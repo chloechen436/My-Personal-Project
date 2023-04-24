376434d824d73802a7d53e5f3b85d02078adfe39
Janice <jadeng3@s.sfusd.edu>
2023-04-24 12:06:18 -0700

    tried to add script to score

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+
+    public TextMeshProUGUI scoreText;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score: 0;
+        scoreText.text = "Score:" + score;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
