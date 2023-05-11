4e84c64fc508760939f6a44e80f39ea8cc17f729
peeryo <40718018+peeryo@users.noreply.github.com>
2023-05-10 18:37:00 -0700

    fixed the missing score text

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,7 +12,7 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        score: 0;
+        score = 0;
         scoreText.text = "Score:" + score;
         
     }
